574b4442ba4a2b0bd46e30adf81007c08ab73b51
dchair <a0913809823@gmail.com>
2024-12-31 18:29:55 +0800

    jar和連線設定更新上傳

--- src/main/java/chair/mahjong_record/CountApplication.java
+++ src/main/java/chair/mahjong_record/CountApplication.java
@@ -8,21 +8,6 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class CountApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-
-        // 讀取環境變數
-        String dbHost = dotenv.get("DB_HOST");
-        String dbPort = dotenv.get("DB_PORT");
-        String dbName = dotenv.get("DB_NAME");
-        String dbUser = dotenv.get("DB_USER");
-        String dbPass = dotenv.get("DB_PASS");
-
-        // 使用讀取的變數來設置資料庫配置
-        System.setProperty("DB_HOST", dbHost);
-        System.setProperty("DB_PORT", dbPort);
-        System.setProperty("DB_NAME", dbName);
-        System.setProperty("DB_USER", dbUser);
-        System.setProperty("DB_PASS", dbPass);
 
         SpringApplication.run(CountApplication.class, args);
 
